980c24c2fd028227bc06c26389ef2a2b2226a3c3
Dennis Peterson <denispeterson96@gmail.com>
2019-04-26 12:46:35 +0300

    Finally Returned all Todos titles

--- lib/all_todos/extract.ex
+++ lib/all_todos/extract.ex
@@ -2,7 +2,8 @@ defmodule Todo.Extract do
   def extract_required(map) do
     # All_titles = []
     {:ok, body} = map
-    IO.puts body
+    Enum.map(body, fn x -> x["title"] end)
+    |> IO.inspect
     # IO.puts "Todo title is: " <> body["title"]
   end
 end
